2aea2d83ee7b5df640094ab3974fcecfc47156d7
Minh Chau <xiu.chau@gmail.com>
2017-09-20 23:15:51 -0700

    refactor: combine routes

--- routes/colors.js
+++ routes/colors.js
@@ -27,23 +27,64 @@ router.get('/colors', (req, res, next) => {
       });
   }
   else {
+    knex('colors')
+      .then(colors => {
+        const matches = [];
+        const rgbValues = colors.map(color => {
+          return convertHex(color.hex);
+        });
 
+        for (let i = 0; i < colors.length; i++) {
+          // define red
+          if (family === 'red' && rgbValues[i].r > 200 && rgbValues[i].g < 95 && rgbValues[i].b < 95) {
+            matches.push(colors[i]);
+          }
+
+          // define green
+          if (family === 'green' && rgbValues[i].r < 150 && rgbValues[i].g > 200 && rgbValues[i].b < 150) {
+            matches.push(colors[i]);
+          }
+
+          // define blue
+          if (family === 'blue' && rgbValues[i].r < 90 && rgbValues[i].g < 200 && rgbValues[i].b > 200) {
+            matches.push(colors[i]);
+          }
+
+          // define purple
+          if (family === 'purple' && rgbValues[i].r > 120 && rgbValues[i].g < 40 && rgbValues[i].b > 120) {
+            matches.push(colors[i]);
+          }
+
+          // define yellow
+          if (family === 'yellow' && rgbValues[i].r > 190 && rgbValues[i].g > 190 && rgbValues[i].b < 50) {
+            matches.push(colors[i]);
+          }
+
+          // define orange
+          if (family === 'orange' && rgbValues[i].r > 200 && rgbValues[i].g > 150 &&  rgbValues[i].g < 200 && rgbValues[i].b < 20) {
+            matches.push(colors[i]);
+          }
+
+          // define brown
+          if (family === 'brown' && rgbValues[i].r > 120 && rgbValues[i].r < 200 && rgbValues[i].g > 65 && rgbValues[i].g < 115 && rgbValues[i].b < 70) {
+            matches.push(colors[i]);
+          }
+
+          // define gray
+          if (family === 'gray' && rgbValues[i].r <= rgbValues[i].g  + 15 && rgbValues[i].r >= rgbValues[i].g  - 15 && rgbValues[i].b <= rgbValues[i].r + 15 && rgbValues[i].b >= rgbValues[i].r - 15) {
+            matches.push(colors[i]);
+          }
+        }
+
+        res.send({
+          colors: matches,
+          count: matches.length
+        });
+      })
+      .catch(err => {
+        next(err);
+      });
   }
-});
-
-router.get('/colors/:start/:limit', (req, res, next) => {
-  const start = req.params.start;
-  const limit = req.params.limit;
-
-  knex('colors')
-    .offset(start)
-    .limit(limit)
-    .then(colors => {
-      res.send(colors);
-    })
-    .catch(err => {
-      next(err);
-    });
 });
 
 router.get('/color/:id', (req, res, next) => {
@@ -76,63 +117,5 @@ router.get('/search', (req, res, next) => {
     });
 });
 
-router.get('/family', (req, res, next) => {
-  const fam = req.query.fam;
-
-  knex('colors')
-    .then(colors => {
-      const matches = [];
-      const rgbValues = colors.map(color => {
-        return convertHex(color.hex);
-      });
-
-      for (let i = 0; i < colors.length; i++) {
-        // define red
-        if (fam === 'red' && rgbValues[i].r > 200 && rgbValues[i].g < 95 && rgbValues[i].b < 95) {
-          matches.push(colors[i]);
-        }
-
-        // define green
-        if (fam === 'green' && rgbValues[i].r < 150 && rgbValues[i].g > 200 && rgbValues[i].b < 150) {
-          matches.push(colors[i]);
-        }
-
-        // define blue
-        if (fam === 'blue' && rgbValues[i].r < 90 && rgbValues[i].g < 200 && rgbValues[i].b > 200) {
-          matches.push(colors[i]);
-        }
-
-        // define purple
-        if (fam === 'purple' && rgbValues[i].r > 120 && rgbValues[i].g < 40 && rgbValues[i].b > 120) {
-          matches.push(colors[i]);
-        }
-
-        // define yellow
-        if (fam === 'yellow' && rgbValues[i].r > 190 && rgbValues[i].g > 190 && rgbValues[i].b < 50) {
-          matches.push(colors[i]);
-        }
-
-        // define orange
-        if (fam === 'orange' && rgbValues[i].r > 200 && rgbValues[i].g > 150 &&  rgbValues[i].g < 200 && rgbValues[i].b < 20) {
-          matches.push(colors[i]);
-        }
-
-        // define brown
-        if (fam === 'brown' && rgbValues[i].r > 120 && rgbValues[i].r < 200 && rgbValues[i].g > 65 && rgbValues[i].g < 115 && rgbValues[i].b < 70) {
-          matches.push(colors[i]);
-        }
-
-        // define gray
-        if (fam === 'gray' && rgbValues[i].r <= rgbValues[i].g  + 15 && rgbValues[i].r >= rgbValues[i].g  - 15 && rgbValues[i].b <= rgbValues[i].r + 15 && rgbValues[i].b >= rgbValues[i].r - 15) {
-          matches.push(colors[i]);
-        }
-      }
-
-      res.send(matches);
-    })
-    .catch(err => {
-      next(err);
-    });
-});
 
 module.exports = router;
